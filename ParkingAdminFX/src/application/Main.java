da633a0a0ec9590d0c78e8315caf140081510a6f
Maitena Zubimendi <maitenazubicia@gmail.com>
2023-06-29 19:54:48 -0300

    Modificaciones en la interfaz

--- ParkingAdminFX/src/application/Main.java
+++ ParkingAdminFX/src/application/Main.java
@@ -1,20 +1,20 @@
 package application;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.application.Application;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
-import javafx.scene.layout.VBox;
+import javafx.scene.control.*;
 import javafx.stage.Stage;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.GridPane;
-import javafx.geometry.Pos;
-import javafx.geometry.Insets;
+import javafx.scene.layout.*;
+import javafx.geometry.*;
+import Domain.*;
 
 public class Main extends Application {
-	
+
 	public void start(Stage primaryStage) {
+		
+// creacion pantalla principal
 		Button button = new Button("Administracion");
 		Button button2 = new Button("Registrar ingreso");
 		Button button3 = new Button("Registrar salida");
@@ -30,60 +30,103 @@ public class Main extends Application {
 		primaryStage.setScene(scene);
 		primaryStage.show();
 
+// Ajustes de pantalla administracion
 		button.setOnAction(e -> {
-			Stage newStage = new Stage();
-			newStage.setTitle("Administracion");
-			VBox administracion = new VBox();
-			Scene admi = new Scene(administracion, 200, 200);
-			newStage.setScene(admi);
-			newStage.show();
-
-		});
-		button2.setOnAction(e -> {
-			Stage newStage = new Stage();
-			newStage.setTitle("Registrar ingreso");
-			GridPane registrarIngreso = new GridPane();
-			registrarIngreso.setPadding(new Insets(10));
-			registrarIngreso.setVgap(4);
-			registrarIngreso.setHgap(9);
+			Stage admiP = new Stage();
+			admiP.setTitle("Administracion");
+			GridPane registrarIngresoA = new GridPane();
+			Scene ingresoA = new Scene(registrarIngresoA, 400, 200);
+			registrarIngresoA.setPadding(new Insets(10));
+			registrarIngresoA.setVgap(4);
+			registrarIngresoA.setHgap(6);
 			
-			Label encargado =new Label ("Datos del encargado");
-			Label encargadoN =new Label ("Nombre");
-			TextField textfield = new TextField();
-			Label encargadoD =new Label ("DNI");
+// Creacion de textos, cuadros de texto y botones
+			Label sucursales = new Label("Sucursales");
+			ComboBox sucursal = new ComboBox();
+			sucursal.setItems(FXCollections.observableArrayList("Sucursal 1", "Sucursal 2", "Sucursal 3"));
+			Label encargado = new Label("Datos del encargado");
+			Label encargadoN = new Label("Nombre");
+			TextField textfield1 = new TextField();
+			Label encargadoD = new Label("DNI");
 			TextField textfield2 = new TextField();
-			Label cliente =new Label ("Datos del cliente");
-			Label clienteN =new Label ("Nombre");
+			Button guardar2 = new Button("Guardar");
+			
+// Agregar los botones a la pantalla y seleccionar su ubicacion
+			registrarIngresoA.add(sucursales, 0, 1);
+			registrarIngresoA.add(sucursal, 2, 1);
+			registrarIngresoA.add(encargado, 0, 4);
+			registrarIngresoA.add(encargadoN, 0, 5);
+			registrarIngresoA.add(textfield1, 2, 5);
+			registrarIngresoA.add(encargadoD, 0, 6);
+			registrarIngresoA.add(textfield2, 2, 6);
+			registrarIngresoA.add(guardar2, 0, 8);
+// Mostrar pagina
+			admiP.setScene(ingresoA);
+			admiP.show();
+		});
+
+// Ajustes de pantalla Registrar ingreso
+		button2.setOnAction(e -> {
+			Stage ingresoP = new Stage();
+			ingresoP.setTitle("Registrar ingreso");
+			GridPane registrarIngresoP = new GridPane();
+			registrarIngresoP.setPadding(new Insets(10));
+			registrarIngresoP.setVgap(4);
+			registrarIngresoP.setHgap(6);
+
+// Creacion de textos, cuadros de texto y botones
+			Label cliente = new Label("Datos del cliente");
+			Label clienteN = new Label("Nombre");
 			TextField textfield3 = new TextField();
-			Label clienteD =new Label ("DNI");
+			Label clienteD = new Label("DNI");
 			TextField textfield4 = new TextField();
-			Label vehiculo =new Label ("Datos del vehiculo");
-			Label modelo =new Label ("Modelo");
+			Label vehiculo = new Label("Datos del vehiculo");
+			Label modelo = new Label("Modelo");
 			TextField textfield5 = new TextField();
-			Label patente =new Label ("Patente");
+			Label patente = new Label("Patente");
 			TextField textfield6 = new TextField();
+			Button guardar = new Button("Guardar");
+
+// Ajustes del boton guardar
+			guardar.setOnAction(evento -> {
+				
+// Obtener los datos que se ingresan
+				String nombreCliente = textfield3.getText();
+				String dniCliente = textfield4.getText();
+				String modeloVehiculo = textfield5.getText();
+				String patenteVehiculo = textfield6.getText();
+				
+
+				// Instancia de la clase Cliente
+				// UsuarioABS datosCliente = new UsuarioABS();
+				// Asignar valores
+				// Cliente.setName(nombreCliente);
+				// Acceder a las variables de Cliente y Vehiculo
+				// String NombreC = Cliente.getName();
+
+			});
 			
-			registrarIngreso.add(encargado, 0, 1);
-			registrarIngreso.add(encargadoN, 2, 1);
-			registrarIngreso.add(textfield, 3, 1);
-			registrarIngreso.add(encargadoD, 2, 2);
-			registrarIngreso.add(textfield2, 3, 2);
-			registrarIngreso.add(cliente, 0, 4);
-			registrarIngreso.add(clienteN, 2, 4);
-			registrarIngreso.add(textfield3, 3, 4);
-			registrarIngreso.add(clienteD, 2, 5);
-			registrarIngreso.add(textfield4, 3, 5);
-			registrarIngreso.add(vehiculo, 0, 7);
-			registrarIngreso.add(modelo, 2, 7);
-			registrarIngreso.add(textfield5, 3, 7);
-			registrarIngreso.add(patente, 2, 8);
-			registrarIngreso.add(textfield6, 3, 8);
-			Scene ingreso = new Scene(registrarIngreso, 400, 300);
-			
-			newStage.setScene(ingreso);
-			newStage.show();
+// Agregar los botones a la pantalla y seleccionar su ubicacion
+			registrarIngresoP.add(cliente, 0, 1);
+			registrarIngresoP.add(clienteN, 2, 1);
+			registrarIngresoP.add(textfield3, 3, 1);
+			registrarIngresoP.add(clienteD, 2, 2);
+			registrarIngresoP.add(textfield4, 3, 2);
+			registrarIngresoP.add(vehiculo, 0, 4);
+			registrarIngresoP.add(modelo, 2, 4);
+			registrarIngresoP.add(textfield5, 3, 4);
+			registrarIngresoP.add(patente, 2, 5);
+			registrarIngresoP.add(textfield6, 3, 5);
+			registrarIngresoP.add(guardar, 0, 7);
+			Scene ingreso = new Scene(registrarIngresoP, 400, 200);
+
+// Mostrar pagina
+			ingresoP.setScene(ingreso);
+			ingresoP.show();
 
 		});
+		
+// Ajustes de pantalla Registrar salida
 		button3.setOnAction(e -> {
 			Stage newStage = new Stage();
 			newStage.setTitle("Registrar Salida");
@@ -97,5 +140,4 @@ public class Main extends Application {
 
 	public static void main(String[] args) {
 		launch(args);
-	}
-}
+	}}
